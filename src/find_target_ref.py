b54a220632a736d526c86e0b6a15c890229be008
Nathan Taylor <nathan.taylor.oss@gmail.com>
2016-09-24 10:56:55 -0700

    clean up

--- src/find_target_ref.py
+++ src/find_target_ref.py
@@ -3,27 +3,27 @@ from argparse import ArgumentParser
 import re
 import requests
 
-pr_id_pattern = re.compile('^https://bitbucket\.org/([^/]+)/([^/]+)/pull-requests/(\d+)$')
+_PR_REST_URL_TEMPLATE = "https://api.bitbucket.org/2.0/repositories/{}/{}/pullrequests/{}"
+_PR_URL_PATTERN = re.compile('^https://bitbucket\.org/([^/]+)/([^/]+)/pull-requests/(\d+)$')
 
 
-def _get_bitbuck_pr_api_url(user_pr_url: str) -> str:
-    match = pr_id_pattern.match(user_pr_url)
+def _get_bitbuck_pr_rest_url(pr_url: str) -> str:
+    match = _PR_URL_PATTERN.match(pr_url)
     if match:
         user = match.group(1)
         repo = match.group(2)
         pr_id = match.group(3)
     else:
-        raise RuntimeError("Url provided does not match known pattern: {}".format(user_pr_url))
+        raise RuntimeError("Url provided does not match known pattern: {}".format(pr_url))
 
-    return "https://api.bitbucket.org/2.0/repositories/{}/{}/pullrequests/{}".format(user, repo,
-                                                                                     pr_id)
+    return _PR_REST_URL_TEMPLATE.format(user, repo, pr_id)
 
 
 def get_target_ref(args) -> str:
     if not args.pr_url:
         return 'origin/master'
 
-    pr_rest_url = _get_bitbuck_pr_api_url(args.pr_url)
+    pr_rest_url = _get_bitbuck_pr_rest_url(args.pr_url)
     response = requests.get(pr_rest_url, auth=(args.auth_user, args.auth_password))
     response.raise_for_status()
     return response.json()['destination']['commit']['hash']
